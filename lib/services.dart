86f77fe830008a9c88bd83c031432607198d3715
denz9993j <denz9993j@gmail.com>
2024-08-30 13:47:50 +0000

    services dart file

--- lib/services.dart
+++ lib/services.dart
@@ -1,18 +1,27 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:myapp/dictonary_model.dart';
 
 class APIservices {
   String baseurl = "https://api.dictionaryapi.dev/api/v2/entries/en";
-  Future<DictonaryModel> fetchData(String word) async {
+  Future<DictonaryModel?> fetchData(String word) async {
     Uri url = Uri.parse("$baseurl$word");
     final response = await http.get(url);
     try {
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
+        return DictonaryModel.fromJson(data[0]);
+      } else {
+        throw Exception("Failure to load meaning");
       }
       ;
     } catch (e) {
-      print(e.toString());
+      if (kDebugMode) {
+        print(e.toString());
+      }
     }
+    return null;
   }
 }
