2b349c17dcc5963e1fe53b6968b5207393f31285
denz9993j <denz9993j@gmail.com>
2024-08-31 08:39:43 +0000

    completed spelling changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/dictonary_homepage.dart';
+import 'package:myapp/dictionary_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DictonaryHomepage(),
+      home: DictionaryHomePage(),
     );
   }
 }
--- lib/services.dart
+++ lib/services.dart
@@ -2,17 +2,17 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
-import 'package:myapp/dictonary_model.dart';
+import 'package:myapp/dictionary_model.dart';
 
 class APIservices {
   static String baseurl = "https://api.dictionaryapi.dev/api/v2/entries/en";
-  static Future<DictonaryModel?> fetchData(String word) async {
+  static Future<DictionaryModel?> fetchData(String word) async {
     Uri url = Uri.parse("$baseurl$word");
     final response = await http.get(url);
     try {
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
-        return DictonaryModel.fromJson(data[0]);
+        return DictionaryModel.fromJson(data[0]);
       } else {
         throw Exception("Failure to load meaning");
       }
